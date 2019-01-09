34cff3602317e6dbb8f3c72077d920e44ba01a50
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-09 14:26:03 +0800

    tambah fail baru

---
+++ Aplikasi/Kelas/Fail/Papar/cari/z-navtab.php
@@ -0,0 +1,30 @@
+<div class="row">
+<div class="col-2"><div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+	<a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home"
+	role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+	<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile"
+	role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+</div></div>
+<div class="col-10"><div class="tab-content" id="v-pills-tabContent">
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+	<div class="tab-pane fade" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">A</div>
+	<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+<!-- ================================================================================================= -->
+<div class="form-group row">
+	<label for="inputTajuk" class="col-sm-2 control-label">jantina</label>
+	<div class="col-sm-6">
+		<div class="input-group input-group-sm">
+		<select name="senarai_isirumah1[jantina]" class="form-control">
+			<option value="1">1-Lelaki</option>
+			<option value="2">2-Perempuan</option>
+			</select>
+		</div>
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group" -->
+<!-- ================================================================================================= -->
+	</div>
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+</div></div>
+</div>
