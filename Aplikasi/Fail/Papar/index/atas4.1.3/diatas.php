1826766ba706817d0501130d9c6c71432ef7e61a
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-10 17:21:15 +0800

    tambah fail baru

---
+++ Aplikasi/Fail/Papar/index/atas4.1.3/diatas.php
@@ -0,0 +1,42 @@
+<?php
+$bootstrapCSS_413 = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css';
+ $ceruleanCSS_413 = 'https://maxcdn.bootstrapcdn.com/bootswatch/4.1.3/cerulean/bootstrap.min.css';
+ $fontawesome_510 = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css';
+?><!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Belanja Sakan</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="<?php echo $bootstrapCSS_413 ?>" rel="stylesheet" type="text/css">
+<link href="<?php echo $fontawesome_510 ?>" rel="stylesheet" type="text/css">
+
+<style type="text/css">
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+.fa-input {font-family: FontAwesome}
+</style>
+</head>
+<body>
