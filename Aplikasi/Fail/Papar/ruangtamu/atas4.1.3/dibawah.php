fd60d6f19482f656dc2052604645b4940b823d5c
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-10 17:21:36 +0800

    tambah fail baru

---
+++ Aplikasi/Fail/Papar/ruangtamu/atas4.1.3/dibawah.php
@@ -0,0 +1,17 @@
+<!-- Footer
+================================================== -->
+<footer class="footer"><div class="container"><span class="label label-info">
+&copy; Hak Cipta Terperihara 2018. Theme Asal Bootstrap 4.1.3 dan
+Font Awesome 5.1.0
+</span></div></footer>
+
+<!-- khas untuk jquery dan js2 lain
+<?php
+$jquery_cdn = 'https://code.jquery.com/jquery-2.2.3.min.js';
+$bootstrapJS_413 = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js';
+?>
+================================================== -->
+<script type="text/javascript" src="<?php echo $jquery_cdn ?>"></script>
+<script type="text/javascript" src="<?php echo $bootstrapJS_413 ?>"></script>
+</body>
+</html>
